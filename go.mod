1c53708bbcce52319e41d2227c90aaa53ac98ed0
glim2485 <gjlim2485@gmail.com>
2024-04-19 13:35:46 +0900

    added logging function for tracking results

--- go.mod
+++ go.mod
@@ -19,14 +19,20 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/xuri/efp v0.0.0-20231025114914-d1ff6096ae53 // indirect
+	github.com/xuri/excelize/v2 v2.8.1 // indirect
+	github.com/xuri/nfp v0.0.0-20230919160717-d98342af3f05 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"gjlim2485/bandwidthawarecaching/common"
 	"gjlim2485/bandwidthawarecaching/server"
 	"gjlim2485/bandwidthawarecaching/user"
 	"sync"
+	"time"
+
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
@@ -12,8 +16,49 @@ func main() {
 	var wg sync.WaitGroup
 	common.UserNumbers = 100
 	wg.Add(common.UserNumbers)
+	startTime := time.Now()
 	for i := 0; i < common.UserNumbers; i++ {
 		go user.CreateUserThread(&wg, i)
 	}
 	wg.Wait()
+
+	//log data to excel sheet
+	f, err := excelize.OpenFile("dataLog.xlsx")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	t := time.Now()
+	totalTime := t.Sub(startTime)
+	year, month, day := t.Date()
+	hour, minute := t.Hour(), t.Minute()
+	sheetName := fmt.Sprintf("%d-%d-%d %d:%d", year, month, day, hour, minute)
+
+	index, err := f.NewSheet(sheetName)
+	f.SetCellValue(sheetName, "A1", "UserID")
+	f.SetCellValue(sheetName, "B1", "ItemName")
+	f.SetCellValue(sheetName, "C1", "CacheHit")
+	f.SetCellValue(sheetName, "D1", "TimeTaken")
+	cellIndex := 1
+	for i, d := range common.UserLogInfo {
+		for _, y := range d.UserResult {
+			cellIndex++
+			f.SetCellValue(sheetName, fmt.Sprintf("A%d", cellIndex), i)
+			f.SetCellValue(sheetName, fmt.Sprintf("B%d", cellIndex), y.ItemName)
+			f.SetCellValue(sheetName, fmt.Sprintf("C%d", cellIndex), y.CacheHit)
+			f.SetCellValue(sheetName, fmt.Sprintf("D%d", cellIndex), y.TimeTaken)
+		}
+	}
+	cellIndex++
+	totalDurationMilliseconds := int64(totalTime / time.Millisecond)
+	f.SetCellValue(sheetName, fmt.Sprintf("A%d", cellIndex), "Total Duration")
+	f.SetCellValue(sheetName, fmt.Sprintf("B%d", cellIndex), fmt.Sprintf("%d ms", totalDurationMilliseconds))
+	f.SetActiveSheet(index)
+	if err = f.Save(); err != nil {
+		fmt.Println(err)
+	}
 }
